6f7c3262525edf129e4d8ce01a3097ae29404c89
nikhildevassia <nikhildevasia2000@gmail.com>
2022-07-21 16:32:51 +0530

    vendor

--- vendor/views.py
+++ vendor/views.py
@@ -42,7 +42,7 @@ def vendor_home(request):
     sales = OrderProduct.objects.filter(product__vendor = request.user).count()
     total_price = OrderProduct.objects.filter(product__vendor = request.user).aggregate(Sum('product_price'))
     total = (total_price["product_price__sum"])
-    profit = (total * 0.9)
+    profit = float(total * 0.9)
     context = {
         'sales':sales,
         'profit':profit,
