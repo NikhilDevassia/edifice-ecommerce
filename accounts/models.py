01f2393119ad26b9ad04621d286eee89c50cd1af
nikhildevassia <nikhildevasia2000@gmail.com>
2022-07-21 17:22:25 +0530

    admin

--- accounts/models.py
+++ accounts/models.py
@@ -36,7 +36,6 @@ class MyAccountManager(BaseUserManager):
 
         user.is_admin      = True
         user.is_active     = True
-        user.is_staff      = True
         user.is_superadmin = True
         user.save(using=self._db)
         return user  
--- edifice/settings.py
+++ edifice/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
-"""
+""" 
 
 
 from ctypes import cast
